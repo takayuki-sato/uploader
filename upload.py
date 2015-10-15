2e065c01165e138b8a87b39893e3a7852c161258
alvaro <alvaroabascar@gmail.com>
2015-10-15 14:00:45 +0200

    more logs

--- upload.py
+++ upload.py
@@ -62,10 +62,11 @@ def worker(q, data_type):
         else:
             target = subject.upload_mri
         # check that zip is fine before trying to upload
+        LOGGER.info("Checking zip file {}...".format(data_file))
         with zipfile.ZipFile(data_file, 'r') as myzip:
             if myzip.testzip():
-                LOGGER.error('Skipping upload for subject {}: {}, zip is\
-                             corrupted.'.format(subject_name, data_file))
+                LOGGER.error("Skipping upload for subject {}: {}, zip is\
+                             corrupted.".format(subject_name, data_file))
                 continue
         LOGGER.info('Uploading data for subject {}: {}'.format(subject_name, data_file))
         target(data_file)
